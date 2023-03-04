32d1dc834110da2b192b46687a92fafb511a8290
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-03-04 09:01:53 +0000

    jin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+  # 他人のユーザー画面に編集できなくするコード
+  before_action :correct_user, only: [:edit]
+
   def index
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @newbook = Book.new
   end
 
   def edit
   end
+
+  # 正しいユーザーのみ
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user.id) unless @user == current_user
+  end
+
 end
