54889339c3a42dad425dd37755c815907eaec642
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-03-07 13:07:08 +0000

    sin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,25 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
